5d002300d74b2d36c686896891722d69a1ff1c23
ZacWarner <zektgn@gmail.com>
2019-12-13 23:40:55 -0800

    finished section 3, on assignment 1

--- react-the-complete-guide/src/App.js
+++ react-the-complete-guide/src/App.js
@@ -8,33 +8,67 @@ class App extends Component {
       { name: 'Max', age: 28 },
       { name: 'manu', age: 29 },
       { name: 'stephanie', age: 26 }
-    ]
+    ],
+    otherState: 'some other value'
+  };
 
-  }
-
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('was clicked');
     this.setState({
       persons: [
-        { name: 'Maximilian', age: 28 },
+        { name: newName, age: 28 },
         { name: 'manu', age: 29 },
+        { name: 'stephanie', age: 27 }
+      ]
+    })
+  };
+
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
         { name: 'stephanie', age: 27 }
       ]
     })
   }
 
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: ' 1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App" >
         <h1>Hi, I'm a react app</h1>
         <p>this is really working!</p>
-        <button onClick={this.switchNameHandler}>switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobby is racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button
+          style={style}
+          onClick={() => this.switchNameHandler('Maximillion!!')}
+        >switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!')}
+          changed={this.nameChangeHandler}>My hobby is racing</Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
       </div>
     );
   }
+
 }
 
 export default App;
+
+
+
